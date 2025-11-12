c814ae930754c3fe0162e250eff1d79f24aa8dec
Noah Keen <45008882+noahkeen222@users.noreply.github.com>
2025-11-12 18:54:26 +0000

    fixing routing issues - hopefully final

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
