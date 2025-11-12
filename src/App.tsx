2b8fd6059993c9c9b5a7f69b209601cafb952a5d
Noah Keen <45008882+noahkeen222@users.noreply.github.com>
2025-11-12 19:02:50 +0000

    Finalizing hashrouting

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <Box component="main" sx={{ flexGrow: 1 }}>
           <Routes>
-            <Route path="/" element={<Navigate to="home-site" replace />} />
+            <Route path="/" element={<Navigate to="home" replace />} />
             <Route path="home" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
