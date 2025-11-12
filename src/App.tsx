ae88a2a2e6512c4e3c9c78f22ffd2889f6f447a9
Noah Keen <45008882+noahkeen222@users.noreply.github.com>
2025-11-12 18:59:42 +0000

    Finalizing hashrouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -14,6 +14,7 @@ function App() {
         <Navbar />
         <Box component="main" sx={{ flexGrow: 1 }}>
           <Routes>
+            <Route path="/" element={<Navigate to="home-site" replace />} />
             <Route path="home" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
