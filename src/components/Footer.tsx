20854650a04b820722168b28b10f470b22ad556c
Noah Keen <45008882+noahkeen222@users.noreply.github.com>
2025-11-13 15:14:25 +0000

    adding more responsiveness to pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ const Footer: FC = () => {
                 component={Link}
                 to="home"
                 color="inherit"
-                underline="hover"
+                underline="none"
               >
                 Home
               </MuiLink>
@@ -43,7 +43,7 @@ const Footer: FC = () => {
                 component={Link}
                 to="about"
                 color="inherit"
-                underline="hover"
+                underline="none"
               >
                 About
               </MuiLink>
@@ -51,7 +51,7 @@ const Footer: FC = () => {
                 component={Link}
                 to="contact"
                 color="inherit"
-                underline="hover"
+                underline="none"
               >
                 Contact
               </MuiLink>
